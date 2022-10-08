1ff60b77ed7e3c255603b8530d72d0b0ace2b9bd
Âu Duy Thông <auduythong1610@gmail.com>
2022-10-08 17:42:05 +0700

    upload source code landing page drinking cocacola

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(pick) {
+  document.querySelector(".pepsi").src = pick;
+}
+
+function changeBgColor(color) {
+  const sec = document.querySelector(".sec");
+  sec.style.background = color;
+}
